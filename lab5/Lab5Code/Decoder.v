44fa92c5abe7588fcc89be5cd8ffb23cd991e23e
danzel-crazy <rickydanzel@gmail.com>
2022-05-25 20:04:48 +0800

    able to compile

--- lab5/Lab5Code/Decoder.v
+++ lab5/Lab5Code/Decoder.v
@@ -27,15 +27,15 @@ always @(*) begin
     //only store and branch no need
     Branch   <= (opcode == 7'b1100011) ? 1'b1 : 1'b0;
     //only branch 
-    Jump = (opcode[2:0] == 3'b111) ? 1'b1 : 1'b0;
+    Jump <= (opcode[2:0] == 3'b111) ? 1'b1 : 1'b0;
     //only jal jalr
-    MemRead = (opcode == 7'b0000011) ? 1'b1 : 1'b0;
+    MemRead <= (opcode == 7'b0000011) ? 1'b1 : 1'b0;
     //only load need
-    MemWrite = (opcode == 7'b0100011) ? 1'b1 : 1'b0;
+    MemWrite <= (opcode == 7'b0100011) ? 1'b1 : 1'b0;
     //only store need
-    ALUSrc = (opcode[6:4] == 3'b001 || opcode[6:4] == 3'b000 || opcode[6:4] == 3'b010) ? 1'b1 : 1'b0;
+    ALUSrc <= (opcode[6:4] == 3'b001 || opcode[6:4] == 3'b000 || opcode[6:4] == 3'b010) ? 1'b1 : 1'b0;
     //choose imm
-    ALUOp = (opcode[6:4] == 3'b011) ? 2'b10 : (opcode[6:4] == 3'b001) ? 2'b11 : (opcode[6:4] == 3'b110) ? 2'b10 : 2'b00;
+    ALUOp <= (opcode[6:4] == 3'b011) ? 2'b10 : (opcode[6:4] == 3'b001) ? 2'b11 : (opcode[6:4] == 3'b110) ? 2'b10 : 2'b00;
 
     MemtoReg <= (opcode[6:5] == 2'b00 || opcode[4:2] == 3'b001)? 1'b1 : 1'b0 ;
     //only load  jalr (check)
