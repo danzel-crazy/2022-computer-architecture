cc7b4b805f129ae4625fba9be3a10133b166bc74
danzel-crazy <rickydanzel@gmail.com>
2022-05-26 19:36:41 +0800

    60 point

--- lab5/Lab5Code/Adder.v
+++ lab5/Lab5Code/Adder.v
@@ -3,10 +3,13 @@
 module Adder(
     input  [32-1:0] src1_i,
     input  [32-1:0] src2_i,
-    output [32-1:0] sum_o
+    output reg [32-1:0] sum_o
 );
 
 /* Write your code HERE */
-assign sum_o = src1_i + src2_i;
+always @(*) begin
+    sum_o <= src1_i + src2_i;
+end
+
 
 endmodule
